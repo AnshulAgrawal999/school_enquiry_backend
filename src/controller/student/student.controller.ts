63406fddc85df7148a0369a35e8f17c6084f1721
anshul <anshulagr799@gmail.com>
2024-11-18 17:45:02 +0530

    naming and spacing

--- src/controller/student/student.controller.ts
+++ src/controller/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common'  ;
+import { Body , Controller, Delete, Get, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common'  ;
 
 import { CreateStudentDto } from 'src/dto/create-student.dto'  ;
  
@@ -6,72 +6,112 @@ import { UpdateStudentDto } from 'src/dto/update-student.dto'  ;
 
 import { StudentService } from 'src/service/student/student.service'  ;
 
-@Controller('student')
+
+@Controller( 'student' )
 export class StudentController {
-   constructor(private readonly studentService: StudentService) { }
 
-@Post()
-   async createStudent(@Res() response, @Body() createStudentDto: CreateStudentDto) {
-  try {
-    const newStudent = await this.studentService.createStudent(createStudentDto);
-    return response.status(HttpStatus.CREATED).json({
-    message: 'Student has been created successfully',
-    newStudent,});
- } catch (err) {
-    return response.status(HttpStatus.BAD_REQUEST).json({
-    statusCode: 400,
-    message: 'Error: Student not created!',
-    error: 'Bad Request'
- });
- }
-}
+  constructor( private readonly studentService: StudentService ) { }
 
-@Patch('/:id')
-async updateStudent(@Res() response,@Param('id') studentId: string,
-@Body() updateStudentDto: UpdateStudentDto) {
-  try {
-   const existingStudent = await this.studentService.updateStudent(studentId, updateStudentDto)  ;
-  return response.status(HttpStatus.OK).json({
-  message: 'Student has been successfully updated',
-  existingStudent,});
- } catch (err) {
-   return response.status(err.status).json(err.response)  ;
- }
-}
-
-@Get()
-async getStudents(@Res() response) {
-try {
-  const studentData = await this.studentService.getAllStudents()  ;
-  return response.status(HttpStatus.OK).json({
-  message: 'All students data found successfully',studentData,})  ;
- } catch (err) {
-  return response.status(err.status).json(err.response)  ;
- }
-}
-
-@Get('/:id')
-async getStudent(@Res() response, @Param('id') studentId: string) {
- try {
-    const existingStudent = await
-this.studentService.getStudent(studentId)  ;
-    return response.status(HttpStatus.OK).json({
-    message: 'Student found successfully',existingStudent,})  ;
- } catch (err) {
-   return response.status(err.status).json(err.response)  ;
- }
-}
-
-@Delete('/:id')
-async deleteStudent(@Res() response, @Param('id') studentId: string)
-{
-  try {
-    const deletedStudent = await this.studentService.deleteStudent(studentId)  ;
-    return response.status(HttpStatus.OK).json({
-    message: 'Student deleted successfully',
-    deletedStudent,});
-  }catch (err) {
-    return response.status(err.status).json(err.response)  ;
+  @Post()
+  async createStudent ( @Res() response , @Body() createStudentDto: CreateStudentDto ) 
+  {
+    try {
+      const newEnquiryForm = await this.studentService.createStudent( createStudentDto )  ;
+  
+      return response.status( HttpStatus.CREATED ).json(
+          {
+              message: 'EnquiryForm has been created successfully'  ,
+              newEnquiryForm
+          }
+      )  ;
+  
+   } catch ( err ) {
+  
+      return response.status( HttpStatus.BAD_REQUEST ).json(
+          {
+              statusCode: 400,
+              message: 'Error: EnquiryForm not created!',
+              error: 'Bad Request'
+          }
+      )  ;
+   }
   }
- }
-}
+
+
+
+  @Patch( '/:id' )
+  async updateStudent( @Res() response , @Param( 'id' ) enquiryFormId: string , @Body() updateStudentDto: UpdateStudentDto ) 
+  {
+    try {
+    const existingEnquiryForm = await this.studentService.updateStudent( enquiryFormId , updateStudentDto )  ;
+  0
+    return response.status( HttpStatus.OK ).json(
+          {
+              message: 'EnquiryForm has been successfully updated',
+              existingEnquiryForm
+          }
+  )  ;
+   } catch (err) {
+  
+     return response.status( err.status ).json( err.response )  ;
+  
+   }
+  }
+
+  @Get( '/:id' )
+  async getStudent( @Res() response , @Param( 'id' ) enquiryFormId: string ) {
+   try {
+      const existingEnquiryForm = await this.studentService.getStudent( enquiryFormId )  ;
+  
+      return response.status( HttpStatus.OK ).json(
+        {
+          message: 'EnquiryForm found successfully'  ,
+          existingEnquiryForm
+        }
+    )  ;
+   } catch ( err ) {
+     return response.status( err.status ).json( err.response )  ;
+   }
+  }
+  
+
+  @Get()
+  async getAllEnquiryForms( @Res() response ) {
+  try {
+  
+    const enquiryFormData = await this.studentService.getAllStudents()  ;
+  
+    return response.status( HttpStatus.OK ).json(
+      {
+          message: 'All enquiry forms data found successfully',
+          enquiryFormData
+      }
+      )  ;
+      } catch (err) {
+  
+      return response.status( err.status ).json( err.response )  ;
+  
+   }
+  }
+
+
+  @Delete( '/:id' )
+  async deleteEnquiryForm( @Res() response , @Param( 'id' ) enquiryFormId: string )
+  {
+    try {
+  
+      const deletedEnquiryForm = await this.studentService.deleteStudent( enquiryFormId )  ;
+  
+      return response.status( HttpStatus.OK ).json(
+        {
+          message: 'EnquiryForm deleted successfully',
+          deletedEnquiryForm
+        }
+      )  ;
+    }catch (err) {
+  
+      return response.status( err.status ).json( err.response )  ;
+  
+    }
+   }
+  }
